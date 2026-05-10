c075ba8dedd0a3d86505eec6e77070baa3458c7f
Henry <henry.arcangello@gmail.com>
2026-05-10 04:22:55 +0100

    pdf export fix

--- transactions/views.py
+++ transactions/views.py
@@ -6,6 +6,7 @@ from decimal import Decimal
 from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
+from django.template.loader import render_to_string
 from django.views.decorators.http import require_http_methods
 from weasyprint import HTML
 
